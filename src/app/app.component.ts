3afe4687aa99067619ea68589cc1ace3e745ce1b
neoLeo123 <113144967+neoLeo123@users.noreply.github.com>
2023-10-26 17:59:39 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { GameService } from './game.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tic-tac-toe';
+  constructor(public gameService: GameService){}
+
+  resetGame(){
+    this.gameService.newGame();
+  }
+}
---
+++ src/app/square/square.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { GameService } from '../game.service';
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.css']
+})
+
+export class SquareComponent implements OnInit {
+  constructor( public gameService: GameService){}
+
+  @Input() square: any;
+
+  ngOnInit(): void{
+  }
+
+  changePlayer(){
+    this.gameService.isGameRunning = true;
+    
+    if(this.gameService.isGameRunning && this.square.state === null){
+      this.square.state= this.gameService.activePlayer;
+      this.gameService.changePlayerTurn(this.square);
+    }
+  }
+
+
+}
